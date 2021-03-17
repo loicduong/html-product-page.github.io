79ba149ba7581c63672b9cf6f86371ce2535c474
dploc96 <mikhailcolt@gmail.com>
2021-03-17 14:01:23 +0700

    fixed css slider img

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 var slideIndex = 1;
 
-function showSlider(n) {
-    var i;
-    var slider = document.getElementsByClassName("slider__item");
-    var dots = document.getElementsByClassName("slider__dot");
-    if (n > slider.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slider.length}
-    for (i = 0; i < slider.length; i++) {
-        slider[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slider[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+const showSlider = (n) => {
+  let i;
+  const slider = document.getElementsByClassName('slider__item');
+  const dots = document.getElementsByClassName('slider__dot');
+  if (n > slider.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slider.length;
+  }
+  for (i = 0; i < slider.length; i++) {
+    slider[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slider[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+};
 
-function plusSlider(n) {
-    showSlider(slideIndex += n);
-}
+const plusSlider = (n) => showSlider((slideIndex += n));
 
-function currentSlider(n) {
-    showSlider(slideIndex = n);
-}
+const currentSlider = (n) => showSlider((slideIndex = n));
 
 showSlider(slideIndex);
-
-
-
